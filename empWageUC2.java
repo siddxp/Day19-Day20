296b0a60c2d177f4c4ac1c022c4b4bad587e5c15
siddxp <siddhantk071@gmail.com>
2020-08-03 08:51:39 +0530

    UC--2-Daily wage added

---
+++ empWageUC2.java
@@ -0,0 +1,19 @@
+public class empWageUC2 {
+   public static void main(String[] args) {
+      int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		int empHours=0;
+		int empWage=0;
+      double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME)
+			{
+				empHours=8;
+			}
+		else
+			{
+				empHours=0;
+			}
+		empWage=empHours*EMP_RATE_PER_HOUR;
+		System.out.println("empWage:"+empWage);
+      	}
+}
