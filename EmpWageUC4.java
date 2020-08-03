77f6fc9fc63da080e429d630df9718bf5064d680
siddxp <siddhantk071@gmail.com>
2020-08-03 09:02:57 +0530

    UC--4-Switch Case Added

---
+++ EmpWageUC4.java
@@ -0,0 +1,23 @@
+public class EmpWageUC4 {
+	   public static final int IS_FULL_TIME=1;
+      public static final int IS_PART_TIME=2;
+      public static final int EMP_RATE_PER_HOUR=20;
+
+   public static void main(String[] args) {
+      int empHours=0;
+      int empWage=0;
+      int empCheck= (int) Math.floor(Math.random()*10)%3;
+      switch (empCheck) {
+			case IS_FULL_TIME:
+            empHours=8;
+				break;
+      	case IS_PART_TIME:
+            empHours=4;
+				break;
+         default:
+            empHours=0;
+           }
+      empWage=empHours*EMP_RATE_PER_HOUR;
+      System.out.println("empWage:"+empWage);
+         }
+}
